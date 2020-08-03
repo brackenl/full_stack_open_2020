f4e33e6c1a8e77b8571ba31b7ffc89d176250741
William Bracken <liam.bracken@gmail.com>
2020-08-03 15:04:02 +0100

    Part 4

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -1,19 +1,78 @@
 const app = require("express");
+const jwt = require("jsonwebtoken");
 const blogsRouter = app.Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
+// GET
+
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
+  response.json(blogs);
 });
 
-blogsRouter.post("/", (request, response) => {
-  const blog = new Blog(request.body);
+// POST
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
+blogsRouter.post("/", async (request, response) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+
+  const user = await User.findById(decodedToken.id);
+
+  const blog = new Blog({
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes || 0,
+    user: user._id,
   });
+  const result = await blog.save();
+  user.blogs = user.blogs.concat(result._id);
+  await user.save();
+  response.status(201).json(result);
+});
+
+// DELETE
+
+blogsRouter.delete("/:id", async (request, response) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+
+  const relBlog = await Blog.findById(request.params.id);
+
+  if (decodedToken.id.toString() !== relBlog.user.toString()) {
+    return response
+      .status(403)
+      .json({ error: "Only the author of a blog post may delete it." });
+  }
+
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
+
+// PUT
+
+blogsRouter.put("/:id", async (request, response) => {
+  const blog = request.body;
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+  response.json(updatedBlog);
 });
 
 module.exports = blogsRouter;
--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -8,18 +8,34 @@ const requestLogger = (req, res, next) => {
   next();
 };
 
-const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: "unknown endpoint" });
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    request.token = authorization.substring(7);
+  }
+  next();
 };
 
-const errorHandler = (error, req, res, next) => {
-  logger.error(error.message);
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-  if (error.name === "CastError" && error.kind === "ObjectId") {
-    return res.status(400).send({ error: "malformatted id" });
+const errorHandler = (error, request, response, next) => {
+  if (error.name === "CastError") {
+    return response.status(400).send({
+      error: "malformatted id",
+    });
   } else if (error.name === "ValidationError") {
-    return res.status(400).json({ error: error.message });
+    return response.status(400).json({
+      error: error.message,
+    });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(401).json({
+      error: "invalid token",
+    });
   }
+
+  logger.error(error.message);
 
   next(error);
 };
@@ -28,4 +44,5 @@ module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
 };
